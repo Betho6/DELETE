285c396480105b5c6ed80f66f9c7343d533bf9b0
Ben Thomas <benthomas462@gmail.com>
2021-01-13 17:28:00 -0800

    added transitioning photos of Eli Manning

--- src/React/Main.jsx
+++ src/React/Main.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import react, {useState} from 'react';
 
-/* Components ---------------------------*/
-import Test from './Test.jsx';
+const Main =() => {
+    const [imageSrc, imageSrcUpdate] = useState ('/assets/img/eli_normal.jpg');
 
-const Main = () => {
-
+const imageOver =( ) => {
+    imageSrcUpdate('/assets/img/eli_wacky.jpg');
+}
+const imageOut =( ) => {
+    imageSrcUpdate('/assets/img/eli_normal.jpg');
+}
     return (
-        <div className='Main'>
-            <h1>Ben's Final</h1>
-        </div>
-    );
+        <div1>
+            <img src={ imageSrc }
+            onMouseOver={imageOver}
+            onMouseOut={imageOut}
+            />
+            <p>I was an elite NFL Quarterback</p>
+        </div1>
+    )
 }
 
-export default Main;
+export default Main ;
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import ReactDOM from 'react-dom';
 | Components
 ---------------------------*/
 import Main from './React/Main.jsx';
+import TransitioningPictures from 'React/Main.jsx';
 
 /*---------------------------
 | Render to DOM
